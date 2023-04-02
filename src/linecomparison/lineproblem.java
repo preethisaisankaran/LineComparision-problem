4561c479d66846d1c73fcc1280457e5d766a8526
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-02 12:35:11 +0530

    compare line with points

---
+++ src/linecomparison/lineproblem.java
@@ -0,0 +1,37 @@
+package linecomparison;
+
+public class lineproblem {
+    public static void main(String[] args) {
+        int line;
+        int l1.point ep1;
+    }
+
+    line l1 = new line();
+    l1.ep1=new
+
+    point();
+
+    l1.ep1.x=6;
+    l1.ep1.y=60;
+    l1.ep1=new point();
+
+    l1.ep2.x=70;
+    l1.ep2.y=200;
+    float length1 = l1.calculateLength();
+        System.out.println("length1:"+length1);
+
+    line l2 = new line();
+    l2.ep1=new point();
+
+    l2.ep1.x=7;
+    l2.ep1.y=70;
+    l2.ep2=new point();
+
+    l2.ep2.x=80;
+    l2.epe2.y=200;
+    float length2 = l2.calculateLength();
+}
+
+
+
+
